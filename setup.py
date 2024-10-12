2121500c343f22f914c73b5ad447bfddc950bdfc
Mike Buhain <mikebuhain.copypasta@vanguards.group>
2024-10-12 22:46:41 +0800

    fixed dependency

--- setup.py
+++ setup.py
@@ -5,11 +5,13 @@ setup(
     version="0.1.001",
     packages=find_packages(),
     install_requires=[
-        "git+https://github.com/Valcrist/toolbox.git",
         "playwright",
         "bs4",
         "aiohttp",
         "aiofiles",
+    ],
+    dependency_links=[
+        "git+https://github.com/Valcrist/toolbox.git#egg=toolbox",
     ],
     url="https://github.com/Valcrist/webscraper",
     author="Valcrist",
